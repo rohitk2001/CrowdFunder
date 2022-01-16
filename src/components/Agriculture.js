87fcbc59750fcce1600d4a1154a5fc5ddad87db5
shashank katoch <shashankkatoch@shashanks-MacBook-Air.local>
2022-01-16 16:58:28 +0400

    added some other related webpages

--- src/components/Agriculture.js
+++ src/components/Agriculture.js
@@ -1,13 +1,15 @@
 import { Fragment } from "react";
+import FeaturedPost from "./FeaturedPost";
 
 
 
 const Agriculture = () => {
     return (
         <Fragment>
-           <h1>
-           Agriculture page
-           </h1>
+            <h1>Agriculture page</h1>
+            <FeaturedPost date = "12 nov" description = "a card" image = "../partner/image/3.jpg" imageLabel = "nothing" title = "hello" />
+            <FeaturedPost date = "12 nov" description = "a card" image = "../partner/image/3.jpg" imageLabel = "nothing" title = "hello" />
+            <FeaturedPost date = "12 nov" description = "a card" image = "../partner/image/3.jpg" imageLabel = "nothing" title = "hello" />
         </Fragment>
     );
 
