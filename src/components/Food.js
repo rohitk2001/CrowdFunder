41be8410c9ff710b252cd6a60c1edf2836a414e1
shashank katoch <shashankkatoch@shashanks-MacBook-Air.local>
2022-01-16 14:57:22 +0400

    added new webpages

---
+++ src/components/Food.js
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+
+
+
+const Food = () => {
+    return (
+        <Fragment>
+           <h1>
+           Food page
+           </h1>
+        </Fragment>
+    );
+
+};
+
+export default Food;
---
+++ src/components/Technology.js
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+
+
+
+const Technology = () => {
+    return (
+        <Fragment>
+           <h1>
+           Technology page
+           </h1>
+        </Fragment>
+    );
+
+};
+
+export default Technology;
