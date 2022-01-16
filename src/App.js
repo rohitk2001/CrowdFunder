0b1ccca7162d943566e0b7004a5d433d00a4b119
Rohit Khandelwal <rohitkhandelwal2001@gmail.com>
2022-01-16 14:29:55 +0530

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-
+import About from './About';
+import Category from './Category';
+import PartnerSlider from './partner/partnerSlider';
+import Footer from './Footer/footer';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <About/>
+      <Category/>
+      <PartnerSlider/>
+      <Footer/>
     </div>
   );
 }
